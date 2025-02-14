c05046130a08e84ef78b6b9313532c7207662ca6
Heosve <juanjosehenao1810@gmail.com>
2025-02-14 08:35:15 -0500

    Add project files.

---
+++ WebApplication1/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models
+{
+    public class Client
+    {
+        public int Id { get; set; } // Id del cliente
+        public string Name { get; set; } // Nombre del cliente
+        public string Email { get; set; } // Correo del cliente
+        public string Phone { get; set; } // Teléfono del cliente
+        public string Age { get; set; }// Edad del cliente
+        public string Address { get; set; } // Dirección del cliente
+
+    }
+}
